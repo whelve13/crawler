18a479f852679927e6cc20aee53339f819dff303
whelve <132349552+whelve13@users.noreply.github.com>
2026-08-21 23:27:38 +0300

    fix: disable CORS allow_credentials to support wildcard origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,7 +32,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.BACKEND_CORS_ORIGINS,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
